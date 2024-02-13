d0166be6dd49bc6dc6af76c845cf42e2109a4bfb
Michael Bestas <mkbestas@lineageos.org>
2024-02-13 13:48:13 +0200

    bluejay: Enable Osaifu-Keitai on non-Japanese SKUs
    Change-Id: Iddb9bb9cd2b81fa384bef22fbf0fb22ee0af7dff

--- bluejay-vendor.mk
+++ bluejay-vendor.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     LLKAgent \
     MyVerizonServices \
     OemRilService \
-    PixelNfc \
     RilConfigService \
     ShannonIms \
     ShannonQualifiedNetworksService \
